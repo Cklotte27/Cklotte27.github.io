7430050f43a571ca4bf7ba1e12b89a99a3b276bf
Chankyung Jung <chankyung0327@gmail.com>
2021-10-05 14:47:06 +0000

    edited vue.config.js file

--- ckport/vue.config.js
+++ ckport/vue.config.js
@@ -2,9 +2,9 @@ module.exports = {
   devServer: {
     //compress: true,
     disableHostCheck: true,
-	publicPath: process.env.NODE_ENV === 'production'
-	  ? '/cklotte27.github.io/'
-	  : '/',
+	//publicPath: process.env.NODE_ENV === 'production'
+	  //? '/cklotte27.github.io/'
+	  //: '/',
     //outputDir: 'docs'
 
  }   
