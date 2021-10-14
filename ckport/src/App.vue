a6f3986174f07a883fdc7e3f0c1d6874e4f08cd5
Chankyung Jung <chankyung0327@gmail.com>
2021-10-14 13:02:49 +0000

    initial changes

--- ckport/src/App.vue
+++ ckport/src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Cklotte Portfolio"/>
-  </div>
+	<div id="app">
+		<Header></Header>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	import Header from './components/header/Header.vue';
+	export default {
+		name: 'App',
+		components: {
+			Header,
+		},
+		created: function(){
+			//console.log(this);
+		}
+	};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	html, body {
+		margin: 0;
+	}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		background-color: #192231;
+		height: 100vh;
+	}
 </style>
--- ckport/vue.config.js
+++ ckport/vue.config.js
@@ -1,13 +1,26 @@
 module.exports = {
-  devServer: {
+  	devServer: {
     //compress: true,
-    disableHostCheck: true,
+		disableHostCheck: true,
+	//overlay: false
 	//publicPath: process.env.NODE_ENV === 'production'
 	  //? '/cklotte27.github.io/'
 	  //: '/',
     //outputDir: 'docs'
-	publicPath: '/cklotte27.github.io',
-	outputDir: 'docs'
+	//publicPath: '/cklotte27.github.io', //maybe I need it to make github pages work but I don't think so.
+	//outputDir: 'docs'
 
- }   
+ 	},
+	/*css: { // tried using stylus but failed
+        loaderOptions: {
+            stylus: {
+                use: [
+                    'vue-style-loader',
+    				'css-loader',
+    				'stylus-loader'
+                ],
+            },
+        },
+	}	
+	*/
 }
